793a25bc12c62e46cbce1dd22f841c56294eb1eb
Gregor Dzierzon <gregor.dzierzon@techelevator.com>
2021-03-04 23:05:41 -0700

    modified user permission database scripts

--- final-capstone/java/database/create_capstone_database.sh
+++ final-capstone/java/database/create_capstone_database.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 BASEDIR=$(dirname $0)
-DATABASE=final_capstone
+DATABASE=meal_planner
 export PGPASSWORD='postgres1'
 
 psql -U postgres -f "$BASEDIR/01_drop_and_create_db.sql" &&
-psql -U postgres -d $DATABASE -f "$BASEDIR/02_create_database_users.sql" &&
 psql -U postgres -d $DATABASE -f "$BASEDIR/03_create_table_schema.sql" &&
-psql -U postgres -d $DATABASE -f "$BASEDIR/04_insert_seed_data.sql"
+psql -U postgres -d $DATABASE -f "$BASEDIR/04_insert_seed_data.sql" &&
+psql -U postgres -d $DATABASE -f "$BASEDIR/05_create_database_users.sql"
