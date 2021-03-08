76e79cb466e05c261671c1ed9136730226bf636d
Justin Bollinger <jbollingerinterpreter@gmail.com>
2021-03-08 15:00:43 -0500

    Java Checkpoint

--- final-capstone/java/src/main/java/com/techelevator/Application.java
+++ final-capstone/java/src/main/java/com/techelevator/Application.java
@@ -3,27 +3,14 @@ package com.techelevator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class Application
 {
-	
-	private static final String API_BASE_URL = "http://localhost:8080/";
-	
-	private static final String LOGIN_MENU_OPTION_LOGIN = "Login";
-	private static final String LOGIN_MENU_OPTION_REGISTER = "Register";
-	private static final String MENU_OPTION_EXIT = "Exit";
-	private static final String[] LOGIN_MENU_OPTIONS = {LOGIN_MENU_OPTION_LOGIN, 
-														LOGIN_MENU_OPTION_REGISTER,
-														MENU_OPTION_EXIT};
 
 	public static void main(String[] args)
 	{
 		SpringApplication.run(Application.class, args);
 	}
 
-	public void run()
-	{
-		System.out.println("***** Hi *******");
-	}
-	
 }
