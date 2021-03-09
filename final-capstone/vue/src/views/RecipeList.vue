2b7c9cf03a27f68fd45441bfadbac09c0a1d4088
Sarah Sobel <sobelsl@gmail.com>
2021-03-09 18:53:41 -0500

    Revised recipeList

--- final-capstone/vue/src/views/RecipeList.vue
+++ final-capstone/vue/src/views/RecipeList.vue
@@ -3,11 +3,9 @@
     <h1>Recipe Library</h1>
     <img src="../assets/Artboard 5.png" alt="full bag of groceries">
     <div class="recipe-list">
-    <div v-for="recipe in recipes" v-bind:key="recipe.id" class="recipe">
-      <router-link v-bind:to="{name: 'RecipeList', params:{id: recipe.id}}">
-      {{ recipe.name }}
-      </router-link>
-    </div>
+      <div v-for="recipe in recipes" v-bind:key="recipe.id" class="recipe">
+        <router-link v-bind:to="{name: 'RecipeList', params:{id: recipe.id}}"> {{ recipe.name }} </router-link>
+      </div>
   </div>
   </div>
 </template>
