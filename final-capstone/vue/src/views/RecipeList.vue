3c76b0eb2fbf2631c1387d0b062a541d13d3e652
Sarah Sobel <sobelsl@gmail.com>
2021-03-08 22:58:27 -0500

    Edits to home, login and recipelist views

--- final-capstone/vue/src/views/RecipeList.vue
+++ final-capstone/vue/src/views/RecipeList.vue
@@ -1,5 +1,15 @@
 <template>
-  <div></div>
+  <div class="recipes">
+    <h1>Recipe Library</h1>
+    <img src="../assets/Artboard 5.png" alt="full bag of groceries">
+    
+    <!-- Display table of recipes - search by... filter by...
+    (do we want each recipe to be a card or in a table format? would be kinda cool to show as a card like pinterest?)
+    
+    Add link to enter new recipe (component - addNewRecipe)
+    Routes to RecipeDetail - then submit new recipe details (using RecipeService?) so that it's then added to recipe list -->
+    
+  </div>
 </template>
 
 <script>
