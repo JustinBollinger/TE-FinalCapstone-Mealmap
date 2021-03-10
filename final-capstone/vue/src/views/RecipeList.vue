011f5a2979588d1bd7c194a65028e4befc876329
Sarah Sobel <sobelsl@gmail.com>
2021-03-09 23:13:45 -0500

    edits to recipeList and recipeService

--- final-capstone/vue/src/views/RecipeList.vue
+++ final-capstone/vue/src/views/RecipeList.vue
@@ -2,50 +2,19 @@
   <div class="recipes">
     <h1>Recipe Library</h1>
     <img src="../assets/Artboard 5.png" alt="full bag of groceries">
+
     <div class="recipe-list">
-      <div v-for="recipe in recipes" v-bind:key="recipe.id" class="recipe">
-        <router-link v-bind:to="{name: 'RecipeList', params:{id: recipe.id}}"> {{ recipe.name }} </router-link>
-      </div>
+      <ul>
+        <li v-for="recipe in recipes" v-bind:key="recipe.id">
+          {{recipe.name}}&<nbsp>|</nbsp>
+          {{recipe.cookingTime}}&<nbsp>|</nbsp>
+          {{recipe.difficulty}}&<nbsp>|</nbsp>
+          {{recipe.dietaryRestrictionId}}&<nbsp>|</nbsp>
+        </li>
+      </ul>
+    </div>
   </div>
 
-<table class="table table-hover">
-  <thead>
-    <tr>
-      <th scope="col">Recipe Name</th>
-      <th scope="col">Total Cook Time</th>
-      <th scope="col">Level of Difficulty</th>
-      <th scope="col">Dietary Restrictions</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">placeholder</th>
-      <td>placeholder</td>
-      <td>placeholder</td>
-      <td>placeholder</td>
-    </tr>
-    <tr class="table-primary">
-      <th scope="row">placeholder</th>
-      <td>placeholder</td>
-      <td>placeholder</td>
-      <td>placeholder</td>
-    </tr>
-    <tr>
-      <th scope="row">placeholder</th>
-      <td>placeholder</td>
-      <td>placeholder</td>
-      <td>placeholder</td>
-    </tr>
-    <tr class="table-primary">
-      <th scope="row">placeholder</th>
-      <td>placeholder</td>
-      <td>placeholder</td>
-      <td>placeholder</td>
-    </tr>
-  </tbody>
-</table>
-
-  </div>
 </template>
 
 <script>
@@ -59,8 +28,8 @@ export default {
     };
   },
   created() {
-    recipeService.list().then((response) => {
-      this.recipes = response.data;
+    recipeService.getRecipes().then((response) => {
+      this.recipes = response.data.responses;
     });
   }
 }
