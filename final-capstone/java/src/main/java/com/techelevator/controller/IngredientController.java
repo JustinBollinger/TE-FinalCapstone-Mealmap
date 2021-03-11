a091008f6a73729a97efe73560247a9e2d5987b7
Justin Bollinger <jbollingerinterpreter@gmail.com>
2021-03-11 18:03:17 -0500

    Updated Ingredient Classes

--- final-capstone/java/src/main/java/com/techelevator/controller/IngredientController.java
+++ final-capstone/java/src/main/java/com/techelevator/controller/IngredientController.java
@@ -1,18 +1,24 @@
 package com.techelevator.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.techelevator.dao.IngredientDAO;
+import com.techelevator.model.Ingredient;
 
 @CrossOrigin
 @RestController
 @PreAuthorize("isAuthenticated()")
 public class IngredientController
 {
-
 	@Autowired
 	private IngredientDAO ingredientDAO;
 	
@@ -21,7 +27,22 @@ public class IngredientController
 		this.ingredientDAO = ingredientDAO;
 	}
 	
-	// API call functions go below
+	@RequestMapping(path = "/ingredientList", method = RequestMethod.GET)
+	public List<Ingredient> listRecipes()
+	{
+		
+	}
+	
+	@RequestMapping(path = "/ingredientList/{ingredient_id}", method = RequestMethod.GET)
+	public Ingredient getIngredientById(@PathVariable int ingredient_id)
+	{
+		return ingredientDAO.getById(ingredient_id);
+	}
 	
 	
+	@RequestMapping(path = "/ingredientList", method = RequestMethod.POST)
+	public Ingredient createIngredient(@RequestBody Ingredient ingredient)
+	{
+		return ingredientDAO.create(ingredient);
+	}
 }
--- final-capstone/java/src/main/java/com/techelevator/dao/IngredientSqlDAO.java
+++ final-capstone/java/src/main/java/com/techelevator/dao/IngredientSqlDAO.java
@@ -1,7 +1,13 @@
 package com.techelevator.dao;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Service;
+
+import com.techelevator.model.Ingredient;
 
 @Service
 public class IngredientSqlDAO implements IngredientDAO
@@ -13,8 +19,62 @@ public class IngredientSqlDAO implements IngredientDAO
 		this.jdbcTemplate = jdbcTemplate;
 	}
 	
-	// SQL query functions go here
+	public List<Ingredient> getAll()
+	{
+		List<Ingredient> ingredients = new ArrayList<>();
+		
+		
+		
+	}
+	
+	public Ingredient getById(int id)
+	{
+		String sql = "SELECT recipe_id" + 
+					", ingredient_id" + 
+					", ingredient_name" + 
+					", category_id" + 
+					" FROM ingredients" + 
+					" WHERE ingredient_id = ?;";
+		
+		SqlRowSet results = jdbcTemplate.queryForRowSet(sql, id);
+		if(results.next())
+		{
+			return mapRowToIngredient(results);
+		}
+		return null;
+	}
 	
 	
+	public Ingredient create(Ingredient newIngredient)
+	{
+		// confirm sql
+		String sql = "INSERT INTO ingredients" + 
+					"(" + 
+					"recipe_id" + 
+					", ingredient_name" + 
+					", category_id" + 
+					")" + 
+					" VALUES" + 
+					"(?, ?, ?)" + 
+					" RETURNING ingredient_id;";
+		
+		Integer id = jdbcTemplate.queryForObject(sql, Integer.class, 
+												newIngredient.getrecipeId(),
+												newIngredient.getIngredientName(),
+												newIngredient.getCategoryId());
+		return getById(id);
+	}
+	
+	
+	private Ingredient mapRowToIngredient(SqlRowSet rowSet)
+	{
+		Ingredient ingredient = new Ingredient();
+		ingredient.setRecipeId(rowSet.getInt("recipe_id"));
+		ingredient.setIngredientId(rowSet.getInt("ingredient_id"));
+		ingredient.setIngredientName(rowSet.getString("ingredient_name"));
+		ingredient.setCategoryId(rowSet.getInt("category_id"));
+		
+		return ingredient;
+	}
 	
 }
