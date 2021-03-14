84ae3329d60d4adc7d685cb8f5e1ddedcd6bd510
Sarah Sobel <sobelsl@gmail.com>
2021-03-14 17:29:54 -0400

    Added placeholder content for meal plan page

---
+++ final-capstone/vue/src/views/MealPlanList.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="mealPlan">
+    <h1>My Meal Plans</h1>
+    <p></p>
+    <img src="../assets/Artboard 5.png" alt="full bag of groceries">
+
+    <div class="mealplan-list">
+      <ul>
+        <li>Meal Plan for Week 1</li>
+        <li>Meal Plan for Week 2</li>
+      </ul>
+    </div>
+  </div>
+  
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+ul{
+  color:white
+}
+
+</style>
