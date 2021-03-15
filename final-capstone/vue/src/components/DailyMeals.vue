24f768f5d8a8b8cc584274fee872206e77641b19
Sarah Sobel <sobelsl@gmail.com>
2021-03-14 22:30:18 -0400

    added meal plan service, daily meal component, added to mealplan list view

---
+++ final-capstone/vue/src/components/DailyMeals.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+      Meal: Breakfast
+        - Recipe 1 by id
+        - Recipe 2 by id
+      Meal: Lunch
+        - Recipe 1 by id
+        - Recipe 2 by id
+      Meal: Dinner
+        - Recipe 1 by id
+        - Recipe 2 by id
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ final-capstone/vue/src/services/MealPlanService.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = process.env.VUE_APP_REMOTE_API;
+
+export default {
+
+  getMeals() {
+    return axios.get('/recipeList')
+  },
+
+  getMealsBy(id) {
+    return axios.get(`/recipeList/recipeDetail/${id}`)
+  },
+
+  create(recipe) {
+    return axios.post(`/recipeList/`, recipe);
+  },
+
+  updateRecipe(id, recipe) {
+    return axios.put(`/recipeList/recipeDetail/${id}`, recipe);
+  },
+
+  // delete(id) {
+  //   return axios.delete(`/recipeList/recipeDetail/${id}`);
+  // }
+
+}
