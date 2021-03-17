01cc7143aa1d7b77d282562fff77741f3bf354b6
Gregor Dzierzon <gregor.dzierzon@techelevator.com>
2021-03-16 23:07:23 -0600

    created started code for displaying and editing the meal plan

---
+++ final-capstone/vue/src/components/MealCard.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="card">
+    <div class="card-body">
+      <h4 class="card-title">{{mealName}}</h4>
+      <div v-if="meal">
+        <ul>
+          <li v-for="recipe in meal.recipes" v-bind:key="recipe.id">
+            <router-link v-bind:to="{name: 'recipe-detail', params: {id: recipe.id}}" >{{recipe.name}}</router-link>
+          </li>
+        </ul>
+      </div>
+      <a href="#" v-on:click.prevent="isAdding = true">Add menu</a>
+      <form v-on:submit.prevent="addMeal" v-if="isAdding">
+        <select>
+          <option value="1">Choose meal to add</option>
+        </select>
+        <button>Add</button>
+      </form>
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MealCard",
+  props: {
+    meal: {},
+    mealName: {},
+    date: {}
+  },
+  data() {
+    return {
+      isAdding: false
+    }
+  },
+  methods: {
+    addMeal() {
+      // add meal to database
+      // update store with the new meal
+      this.isAdding = false;
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
