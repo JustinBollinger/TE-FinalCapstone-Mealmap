e0bb702fda00138e2506ebb6f823d2ccf706ae91
Katrina Moor <moor.katrina@gmail.com>
2021-03-17 18:20:09 -0400

    MP table and data updates

--- final-capstone/java/database/insert_meal_plan_seed_data.sql
+++ final-capstone/java/database/insert_meal_plan_seed_data.sql
@@ -1,155 +1,113 @@
 INSERT INTO meal_plan
 (
-    	user_id
+    user_id
 	, meal_plan_id
 	, meal_plan_name
 	, start_date
+)
+VALUES
+(
+    1
+    , nextval ('seq_meal_plan_id')
+    , 'March Meal Plan'
+    , '2021-03-14'
+);
+
+INSERT INTO meal_plan_recipes
+(
+	meal_plan_id
 	, recipe_id
-	, recipe_name
 	, meal_category
 	, day_of_week
 )
 VALUES
-
 (
     1
-    , 1
-    , 'March Meal Plan'
-    , '2021-03-14'
     , 4
-    , 'Ham and Swiss Omelet'
     , 'Breakfast'
     , 'Sunday'
 ),
 
 (
     1
-    , 1
-    , 'March Meal Plan'
-    , '2021-03-14'
     , 5
-    , 'Peanut Butter Oatmeal'
     , 'Breakfast'
     , 'Monday'
 ),
 
 (
     1
-    , 1
-    , 'March Meal Plan'
-    , '2021-03-14'
     , 6
-    , 'Banana Oatmeal Pancakes'
     , 'Breakfast'
     , 'Tuesday'
 ),
 
 (
     1
-    , 1
-    , 'March Meal Plan'
-    , '2021-03-14'
     , 7
-    , 'Yogurt Parfait'
     , 'Breakfast'
     , 'Wednesday'
 ),
 
 (
     1
-    , 1
-    , 'March Meal Plan'
-    , '2021-03-14'
     , 4
-    , 'Ham and Swiss Omelet'
     , 'Breakfast'
     , 'Thursday'
 ),
 
 (
     1
-    , 1
-    , 'March Meal Plan'
-    , '2021-03-14'
     , 5
-    , 'Peanut Butter Oatmeal'
     , 'Breakfast'
     , 'Friday'
 ),
 
 (
     1
-    , 1
-    , 'March Meal Plan'
-    , '2021-03-14'
     , 6
-    , 'Banana Oatmeal Pancakes'
     , 'Breakfast'
     , 'Saturday'
 ),
 
 (
     1
-    , 1
-    , 'March Meal Plan'
-    , '2021-03-14'
     , 15
-    , 'Chicken Salad'
     , 'Lunch'
     , 'Sunday'
 ),
 
 (
     1
-    , 1
-    , 'March Meal Plan'
-    , '2021-03-14'
     , 16
-    , 'Turkey Spinach Salad'
     , 'Lunch'
     , 'Monday'
 ),
 
 (
     1
-    , 1
-    , 'March Meal Plan'
-    , '2021-03-14'
     , 17
-    , 'Indian Spiced Chickpea Wraps'
     , 'Lunch'
     , 'Tuesday'
 ),
 
 (
     1
-    , 1
-    , 'March Meal Plan'
-    , '2021-03-14'
     , 24
-    , 'Peanut Butter Oatmeal Bars'
     , 'Lunch'
     , 'Tuesday'
 ),
 
 (
     1
-    , 1
-    , 'March Meal Plan'
-    , '2021-03-14'
     , 21
-    , 'Almond Strawberry Salad'
     , 'Lunch'
     , 'Wednesday'
 ),
 
 (
     1
-    , 1
-    , 'March Meal Plan'
-    , '2021-03-14'
     , 22
-    , 'Ultimate Grilled Cheese'
     , 'Lunch'
     , 'Wednesday'
 ),
@@ -157,99 +115,63 @@ VALUES
 
 (
     1
-    , 1
-    , 'March Meal Plan'
-    , '2021-03-14'
     , 19
-    , 'Taco Wraps'
     , 'Lunch'
     , 'Thursday'
 ),
 
 (
     1
-    , 1
-    , 'March Meal Plan'
-    , '2021-03-14'
     , 20
-    , 'White Bean Arugula Salad'
     , 'Lunch'
     , 'Friday'
 ),
 
 (
     1
-    , 1
-    , 'March Meal Plan'
-    , '2021-03-14'
     , 18
-    , 'Sloppy Joes'
     , 'Lunch'
     , 'Saturday'
 ),
 
 (
     1
-    , 1
-    , 'March Meal Plan'
-    , '2021-03-14'
     , 11
-    , 'Slow Cooker Pulled Pork'
     , 'Dinner'
     , 'Sunday'
 ),
 
 (
     1
-    , 1
-    , 'March Meal Plan'
-    , '2021-03-14'
     , 13
-    , 'Oven Roasted Potatoes'
     , 'Dinner'
     , 'Sunday'
 ),
 
 (
     1
-    , 1
-    , 'March Meal Plan'
-    , '2021-03-14'
     , 25
-    , 'Layered Lemon Pie'
     , 'Dinner'
     , 'Sunday'
 ),
 
 (
     1
-    , 1
-    , 'March Meal Plan'
-    , '2021-03-14'
     , 8
-    , 'Tuna Casserole'
     , 'Dinner'
     , 'Monday'
 ),
 
 (
     1
-    , 1
-    , 'March Meal Plan'
-    , '2021-03-14'
     , 9
-    , 'Kielbasa with Peppers and Potatoes'
     , 'Dinner'
     , 'Tuesday'
 ),
 
 (
     1
-    , 1
-    , 'March Meal Plan'
-    , '2021-03-14'
     , 14
-    , 'Steamed Brocoli'
     , 'Dinner'
     , 'Tuesday'
 ),
@@ -258,65 +180,41 @@ VALUES
 (
     1
     , 1
-    , 'March Meal Plan'
-    , '2021-03-14'
-    , 1
-    , 'Vegetarian Stuffed Shells'
     , 'Dinner'
     , 'Wednesday'
 ),
 
 (
     1
-    , 1
-    , 'March Meal Plan'
-    , '2021-03-14'
     , 10
-    , 'Mexican Torte'
     , 'Dinner'
     , 'Thursday'
 ),
 
 (
     1
-    , 1
-    , 'March Meal Plan'
-    , '2021-03-14'
     , 12
-    , 'Loaded Tatertots'
     ,'Dinner'
     , 'Friday'
 ),
 
 (
     1
-    , 1
-    , 'March Meal Plan'
-    , '2021-03-14'
     , 2
-    , 'Green Beans with  Garlic'
     , 'Dinner'
     , 'Saturday'
 ),
 
 (
     1
-    , 1
-    , 'March Meal Plan'
-    , '2021-03-14'
     , 3
-    , 'BLT Sandwich'
     , 'Dinner'
     , 'Saturday'
 ),
 
 (
     1
-    , 1
-    , 'March Meal Plan'
-    , '2021-03-14'
     , 23
-    , 'Chocolate Mousse'
     , 'Dinner'
     , 'Saturday'
 );
--- final-capstone/java/database/test_DB_code.sql
+++ final-capstone/java/database/test_DB_code.sql
@@ -13,6 +13,9 @@ FROM recipes_ingredients;
 SELECT *
 FROM meal_plan;
 
+SELECT *
+FROM meal_plan_recipes;
+
 
 DELETE FROM ingredients
 WHERE ingredient_id >= 43;
