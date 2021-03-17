2aa77848edd5ed2a4ee600d9d0cff777e637df06
Katrina Moor <moor.katrina@gmail.com>
2021-03-17 13:59:43 -0400

    meal plan data

--- final-capstone/java/database/insert_meal_plan_seed_data.sql
+++ final-capstone/java/database/insert_meal_plan_seed_data.sql
@@ -10,6 +10,284 @@ INSERT INTO meal_plan
 	, day_of_week
 )
 VALUES
+
+(
+    1
+    , 1
+    , 'March Meal Plan'
+    , '2021-03-14'
+    , 4
+    , 'Ham and Swiss Omelet'
+    , 'Breakfast'
+    , 'Sunday'
+),
+
+(
+    1
+    , 1
+    , 'March Meal Plan'
+    , '2021-03-14'
+    , 5
+    , 'Peanut Butter Oatmeal'
+    , 'Breakfast'
+    , 'Monday'
+),
+
+(
+    1
+    , 1
+    , 'March Meal Plan'
+    , '2021-03-14'
+    , 6
+    , 'Banana Oatmeal Pancakes'
+    , 'Breakfast'
+    , 'Tuesday'
+),
+
+(
+    1
+    , 1
+    , 'March Meal Plan'
+    , '2021-03-14'
+    , 7
+    , 'Yogurt Parfait'
+    , 'Breakfast'
+    , 'Wednesday'
+),
+
+(
+    1
+    , 1
+    , 'March Meal Plan'
+    , '2021-03-14'
+    , 4
+    , 'Ham and Swiss Omelet'
+    , 'Breakfast'
+    , 'Thursday'
+),
+
+(
+    1
+    , 1
+    , 'March Meal Plan'
+    , '2021-03-14'
+    , 5
+    , 'Peanut Butter Oatmeal'
+    , 'Breakfast'
+    , 'Friday'
+),
+
+(
+    1
+    , 1
+    , 'March Meal Plan'
+    , '2021-03-14'
+    , 6
+    , 'Banana Oatmeal Pancakes'
+    , 'Breakfast'
+    , 'Saturday'
+),
+
+(
+    1
+    , 1
+    , 'March Meal Plan'
+    , '2021-03-14'
+    , 15
+    , 'Chicken Salad'
+    , 'Lunch'
+    , 'Sunday'
+),
+
+(
+    1
+    , 1
+    , 'March Meal Plan'
+    , '2021-03-14'
+    , 16
+    , 'Turkey Spinach Salad'
+    , 'Lunch'
+    , 'Monday'
+),
+
+(
+    1
+    , 1
+    , 'March Meal Plan'
+    , '2021-03-14'
+    , 17
+    , 'Indian Spiced Chickpea Wraps'
+    , 'Lunch'
+    , 'Tuesday'
+),
+
+(
+    1
+    , 1
+    , 'March Meal Plan'
+    , '2021-03-14'
+    , 24
+    , 'Peanut Butter Oatmeal Bars'
+    , 'Lunch'
+    , 'Tuesday'
+),
+
+(
+    1
+    , 1
+    , 'March Meal Plan'
+    , '2021-03-14'
+    , 21
+    , 'Almond Strawberry Salad'
+    , 'Lunch'
+    , 'Wednesday'
+),
+
+(
+    1
+    , 1
+    , 'March Meal Plan'
+    , '2021-03-14'
+    , 22
+    , 'Ultimate Grilled Cheese'
+    , 'Lunch'
+    , 'Wednesday'
+),
+
+
+(
+    1
+    , 1
+    , 'March Meal Plan'
+    , '2021-03-14'
+    , 19
+    , 'Taco Wraps'
+    , 'Lunch'
+    , 'Thursday'
+),
+
+(
+    1
+    , 1
+    , 'March Meal Plan'
+    , '2021-03-14'
+    , 20
+    , 'White Bean Arugula Salad'
+    , 'Lunch'
+    , 'Friday'
+),
+
+(
+    1
+    , 1
+    , 'March Meal Plan'
+    , '2021-03-14'
+    , 18
+    , 'Sloppy Joes'
+    , 'Lunch'
+    , 'Saturday'
+),
+
+(
+    1
+    , 1
+    , 'March Meal Plan'
+    , '2021-03-14'
+    , 11
+    , 'Slow Cooker Pulled Pork'
+    , 'Dinner'
+    , 'Sunday'
+),
+
+(
+    1
+    , 1
+    , 'March Meal Plan'
+    , '2021-03-14'
+    , 13
+    , 'Oven Roasted Potatoes'
+    , 'Dinner'
+    , 'Sunday'
+),
+
+(
+    1
+    , 1
+    , 'March Meal Plan'
+    , '2021-03-14'
+    , 25
+    , 'Layered Lemon Pie'
+    , 'Dinner'
+    , 'Sunday'
+),
+
+(
+    1
+    , 1
+    , 'March Meal Plan'
+    , '2021-03-14'
+    , 8
+    , 'Tuna Casserole'
+    , 'Dinner'
+    , 'Monday'
+),
+
+(
+    1
+    , 1
+    , 'March Meal Plan'
+    , '2021-03-14'
+    , 9
+    , 'Kielbasa with Peppers and Potatoes'
+    , 'Dinner'
+    , 'Tuesday'
+),
+
+(
+    1
+    , 1
+    , 'March Meal Plan'
+    , '2021-03-14'
+    , 14
+    , 'Steamed Brocoli'
+    , 'Dinner'
+    , 'Tuesday'
+),
+
+
+(
+    1
+    , 1
+    , 'March Meal Plan'
+    , '2021-03-14'
+    , 1
+    , 'Vegetarian Stuffed Shells'
+    , 'Dinner'
+    , 'Wednesday'
+),
+
+(
+    1
+    , 1
+    , 'March Meal Plan'
+    , '2021-03-14'
+    , 10
+    , 'Mexican Torte'
+    , 'Dinner'
+    , 'Thursday'
+),
+
+(
+    1
+    , 1
+    , 'March Meal Plan'
+    , '2021-03-14'
+    , 12
+    , 'Loaded Tatertots'
+    ,'Dinner'
+    , 'Friday'
+),
+
 (
     1
     , 1
@@ -18,7 +296,7 @@ VALUES
     , 2
     , 'Green Beans with  Garlic'
     , 'Dinner'
-    , 'Wednesday'
+    , 'Saturday'
 ),
 
 (
@@ -29,5 +307,18 @@ VALUES
     , 3
     , 'BLT Sandwich'
     , 'Dinner'
-    , 'Wednesday'
+    , 'Saturday'
+),
+
+(
+    1
+    , 1
+    , 'March Meal Plan'
+    , '2021-03-14'
+    , 23
+    , 'Chocolate Mousse'
+    , 'Dinner'
+    , 'Saturday'
 );
+
+
